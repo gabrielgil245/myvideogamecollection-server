6a7cb689c2b3e53fd2bf07dcf8afbaa42670f068
gabrielgil245 <gabrielgil245@gmail.com>
2021-10-18 09:51:30 -0500

    initial commit

---
+++ src/main/java/com/revature/myvideogamecollection/MyvideogamecollectionApplication.java
@@ -0,0 +1,13 @@
+package com.revature.myvideogamecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvideogamecollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvideogamecollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/myvideogamecollection/MyvideogamecollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.myvideogamecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyvideogamecollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
